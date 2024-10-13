b2fab4c48d6a37c5a72b92eee16e3eeb5b5bf0c9
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2024-10-13 16:55:13 -0300

    :wrench: fix: entity scan and dependencies

--- src/main/java/com/simplesdental/api/infrastructure/Application.java
+++ src/main/java/com/simplesdental/api/infrastructure/Application.java
@@ -2,8 +2,10 @@ package com.simplesdental.api.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.simplesdental.api.domain")
 public class Application {
 
 	public static void main(String[] args) {
